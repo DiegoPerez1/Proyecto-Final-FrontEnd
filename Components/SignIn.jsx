9665c013a64c2e7812cfdaebd2fb90940c54cd91
DiegoPerez1 <dieguitodieslow@gmail.com>
2023-05-05 15:13:08 -0300

    Actualizando

--- Components/SignIn.jsx
+++ Components/SignIn.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 import '../Styles/SignIn.css'
+import leftArrow from '../src/assets/Vector.svg'
+
 export default class SignIn extends Component {
     render() {
         return (
-            <div></div>
+            <div className='bg'>
+                <section className='arrowSection'>
+                    <img src={leftArrow} alt="" /> <p>Crear cuenta</p>
+                </section>
+                <br />
+                <section>
+                    <h2>¿Cual es tu correo electronico?</h2>
+                </section>
+                <br />
+                <section>
+                    <label htmlFor="">Correo electronico:</label>
+                    <input className='input' type="text" />
+                    <p>Deberias poder confirmarlo luego</p>
+                </section>
+            </div>
         )
     }
 }
