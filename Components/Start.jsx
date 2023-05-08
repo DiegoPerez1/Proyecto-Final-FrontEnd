8ba454cf1f14ecde3bf2aaddbc91d6b761214786
DiegoPerez1 <dieguitodieslow@gmail.com>
2023-05-08 09:21:39 -0300

    Actualizacion de los componentes LogIn y SignIn

--- Components/Start.jsx
+++ Components/Start.jsx
@@ -6,25 +6,10 @@ import googleIcon from "../src/assets/google-icon.svg"
 
 function Start() {
   return (
-<<<<<<< HEAD
-    <div className="container">
-      <section className="top-container">
-        <img src="../src/assets/logo.svg" />
-        <h3>Música a medida.</h3>
-      </section>
-      <section className="bottom-container">
-        <button>Registrarse Gratis</button>
-        <button>Continuar con Google</button>
-        <button>Continuar con Apple</button>
-        <button>Iniciar Sesión</button>
-      </section>
-    </div>
-  )
-=======
     <main id="container">
       <div id="background">
         <section id="top-container">
-          <img id="logo" src={logo}/>
+          <img id="logo" src={logo} />
           <h3 id="title">Música a medida.</h3>
         </section>
 
@@ -32,7 +17,7 @@ function Start() {
           <div id="btn-container">
             <button id="btn-register">Registrarse Gratis</button>
             <div className="login-with">
-              <img  className="icon" src={appleIcon} />
+              <img className="icon" src={appleIcon} />
               <button id="btn-google">Continuar con Google</button>
             </div>
             <div className="login-with">
@@ -45,7 +30,6 @@ function Start() {
       </div>
     </main>
   );
->>>>>>> cbe44992d385d36d98dd6df2df09e66ef2658c9e
 }
 
 export default Start;
