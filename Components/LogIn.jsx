40e0e67967533d42109091be66578eb7dad61508
Rodrigo Costa <ro.costa.dev@gmail.com>
2023-05-09 09:29:15 -0300

    login conectado el backend

--- Components/LogIn.jsx
+++ Components/LogIn.jsx
@@ -1,33 +1,93 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
-import leftArrow from '../src/assets/Vector.svg'
-import '../Styles/Login.css'
+import { Link } from "react-router-dom";
+import leftArrow from "../src/assets/Vector.svg";
+import "../Styles/Login.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-export default class LogIn extends Component {
-    render() {
-        return (
-            <div className='ContainerLogIn'>
-                <section className='header'>
-                    <Link to='/'>  <img src={leftArrow} alt="" className='arrow' /></Link>
-                    <p>Iniciar sesion</p>
-                </section>
-                <br />
-                <section className='titleSection'>
-                    <h2>Nombre de usuario o E-mail</h2>
-                    <input className='input' type="text" />
-                </section>
-                <br />
-                <form className='formSection'>
-                    <section>
-                        <label htmlFor="">Contraseña:</label>
-                        <input className='input' type="text" />
-                    </section>
-                    <section className='buttonSection'>
-                        <button className='button'>Iniciar sesion</button>
-                    </section>
-                </form>
-            </div>
+function Login() {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+  const navigate = useNavigate();
 
-        )
+  const login = async (event) => {
+    event.preventDefault();
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      usuario: user,
+      contraseña: pass,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/login",
+        requestOptions
+      );
+
+      if (response.ok) {
+        const respuesta = await response.json();
+        localStorage.setItem("token", respuesta.token);
+        /*  alert(respuesta.message); */
+        navigate("/nav/home");
+      } else {
+        const respuesta = await response.json();
+        alert(respuesta.error);
+      }
+    } catch (error) {
+      alert(error.message);
     }
+  };
+
+  return (
+    <div className="ContainerLogIn">
+      <section className="headerLogIn">
+        <Link to="/">
+          {" "}
+          <img src={leftArrow} alt="" className="arrow" />
+        </Link>
+        <p>Iniciar sesion</p>
+      </section>
+      <br />
+      <form className="formSectionLogIn" action="submit" onSubmit={login}>
+        <section className="titleSectionLogIn">
+          <h3>Nombre de usuario</h3>
+          <input
+            className="input"
+            type="text"
+            onChange={(e) => {
+              setUser(e.target.value);
+            }}
+            value={user}
+          />
+        </section>
+        <br />
+        <section>
+          <label htmlFor="">Contraseña:</label>
+          <input
+            className="input"
+            type="text"
+            onChange={(e) => {
+              setPass(e.target.value);
+            }}
+            value={pass}
+          />
+        </section>
+        <section className="buttonSectionLogIn">
+          <button className="button" type="submit">
+            Iniciar sesion
+          </button>
+        </section>
+      </form>
+    </div>
+  );
 }
+
+export default Login;
