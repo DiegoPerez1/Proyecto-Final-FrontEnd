a646a73afd248b2d401735f52e665044e3a08e39
DiegoPerez1 <dieguitodieslow@gmail.com>
2023-05-15 22:25:25 -0300

    Cambios varios

--- src/Components/MusicaContextual.jsx
+++ src/Components/MusicaContextual.jsx
@@ -1,168 +1,264 @@
 import "../Styles/MusicaContextual.css";
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import leftArrow from "../assets/Vector.svg";
-import Dropdown from "./Dropdown.jsx";
-import axios from "axios";
-import vector from "../assets/vectorabajo.svg"
+import React, { useEffect, useState } from "react";
+import vector from "../assets/Vector.svg";
 
 function MusicaContextual() {
-    const [selectedOption, setSelectedOption] = useState(null);
-    const [isElectronicaEnabled, setIsElectronicaEnabled] = useState(false);
-    const [isEstiloElectronica, setIsEstiloElectronica] = useState(false);
-    const [actividades, setActividades] = useState([]);
-    const [actividadSeleccionada, setActividadSeleccionada] = useState(null);
-    const [canciones, setCanciones] = useState([]);
-    const [generos, setGeneros] = useState([]);
-    const [generoSeleccionado, setGeneroSeleccionado] = useState({});
+    const [cancionesGeneros, setCancionesGeneros] = useState("");
+    const [actividad, setActividad] = useState("");
+    const [generoSeleccionado, setGeneroSeleccionado] = useState([]);
+    const [actividadGeneroSeleccionados, setActividadGeneroSeleccionados] =
+        useState({});
 
-    useEffect(() => {
-        const obtenerActividades = async () => {
-            try {
-                const respuesta = await axios.get(
-                    "http://localhost:3000/api/actividades"
-                );
-                setActividades(respuesta.data);
-            } catch (error) {
-                console.error(error);
-            }
+    const fetchGeneros = async () => {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", localStorage.getItem("token"));
+        myHeaders.append("Content-Type", "application/json");
+
+        var requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: JSON.stringify({ genero: generoSeleccionado }), // Enviar el género en el body
+            redirect: "follow",
         };
-        obtenerActividades();
-    }, []);
 
-    useEffect(() => {
-        const obtenerGeneros = async () => {
-            if (actividadSeleccionada) {
-                try {
-                    const respuesta = await axios.get(
-                        `http://localhost:3000/api/generos?actividad_id=${actividadSeleccionada}`
-                    );
+        try {
+            const response = await fetch(
+                "http://localhost:3000/api/cancionesporgenero",
+                requestOptions
+            );
 
-                    setGeneros(respuesta.data);
-                } catch (error) {
-                    console.error(error);
-                }
+            if (response.ok) {
+                const data = await response.json();
+                setCancionesGeneros(data);
+                console.log(cancionesGeneros);
             }
-        };
-        obtenerGeneros();
-    }, [actividadSeleccionada]);
-
-    useEffect(() => {
-        const actividadesGuardadas = localStorage.getItem("actividades");
-        if (actividadesGuardadas) {
-            setActividades(JSON.parse(actividadesGuardadas));
-        }
-    }, []);
-
-    const handleChangeActividad = (event) => {
-        const actividadId = parseInt(event.target.value);
-        setActividadSeleccionada(actividadId);
-        localStorage.setItem("actividadSeleccionada", actividadId);
-        const generoAsociado = generos.find(
-            (genero) => genero.actividad_id === actividadId
-        );
-        if (generoAsociado) {
-            setGeneroSeleccionado(generoAsociado.id);
-            localStorage.setItem("generoSeleccionado", generoAsociado.id);
+        } catch (error) {
+            console.error(error);
         }
     };
+    useEffect(() => {
+        if (generoSeleccionado.length > 0) {
+            fetchGeneros();
+        }
+    }, [generoSeleccionado]);
 
-    const handleChangeGenero = (event) => {
-        const generoId = parseInt(event.target.value);
-        setGeneroSeleccionado(generoId);
-        localStorage.setItem("generoSeleccionado", generoId);
+    const handleActividadChange = (event) => {
+        setActividad(event.target.value);
     };
 
-    function handleGeneroSeleccionado(genero) {
+    const handleGeneroClick = (genero) => {
         setGeneroSeleccionado(genero);
-    }
-    function handleSelectChange(selectedOption) {
-        setSelectedOption(selectedOption);
-        setIsElectronicaEnabled(
-            selectedOption !== null && selectedOption.value === "opcion1"
-        );
-    }
+    };
+    const handleCrearPlaylist = () => {
+        if (actividad && generoSeleccionado) {
+            // Guardar la combinación de actividad y género en el estado
+            setActividadGeneroSeleccionados({
+                actividad,
+                genero: generoSeleccionado,
+            });
+            console.log(actividadGeneroSeleccionados);
+        } else {
+            console.log("Debes seleccionar una actividad y un género");
+        }
+    };
+    console.log(generoSeleccionado)
+
 
     return (
         <div id="container">
             <div id="top-gradient"></div>
-            <div id="headerMC">
-                <Link to="/nav/home">
-                    {" "}
-                    <img src={leftArrow} alt="" className="arrowMC" />
-                </Link>
-                Música Contextual
-            </div>
-            <div id="contenedorDropdowns">
-                <div id="contenedorAnimo">
-                    <h1> ¿Cómo te sientes? </h1>
-                    <button id="estadoDeAnimo">
-                        <h7>Estado de Ánimo</h7>
-                        <img id='vector' src={vector}></img>
-                    </button>
-                </div>
-                <div>
-                    <h1 htmlFor="actividades" > ¿Cuál es la ocasión? </h1>
+            <header id="header">
+                <img src={vector} alt="icono de flecha" />
+                <p id="cupido-title">Musica Contextual</p>
+            </header>
+            <div id="inputs-container">
+                <div className="label-input">
+                    <label htmlFor="actividad" id="label-actividad">
+                        ¿Cuál es la ocasión?
+                    </label>
                     <select
-                        id="actividades"
-                        value={actividadSeleccionada || ""}
-                        onChange={handleChangeActividad}>
-                        <option value="">Actividad </option>
-                        {actividades.map((actividad) => (
-                            <option key={actividad.id} value={actividad.id}>
-                                {actividad.nombre}
-                            </option>
-                        ))}
+                        id="actividad"
+                        name="actividad"
+                        onChange={handleActividadChange}
+                    >
+                        <option value="" disabled selected>
+                            Actividad
+                        </option>
+                        <option value="Ejercicio Físico">Ejercicio Fisico</option>
+                        <option value="Limpieza">Limpieza</option>
+                        <option value="Celebracion">Celebracion</option>
+                        <option value="Dormir">Dormir</option>
+                        <option value="Meditar">Meditar</option>
+                        <option value="Social">Social</option>
+                        <option value="Estudiar">Estudiar</option>
+                        <option value="Relajacion">Relajacion</option>
+                        <option value="Viajando">Viajando</option>
                     </select>
                 </div>
-                <div id="contenedorClima">
-                    <h1> ¿Cómo está el clima? </h1>
-                    <button id="clima">
-                        <h7>Clima</h7>
-                        <img id='vector2' src={vector}></img>
-                    </button>
+
+                <div className="label-input">
+                    <label htmlFor="estado" id="label-estado">
+                        ¿Cómo te sientes?
+                    </label>
+                    <select id="estado" name="estado">
+                        <option value="" disabled selected>
+                            Estado de Animo
+                        </option>
+                        <option value="Feliz">Feliz</option>
+                        <option value="Triste">Triste</option>
+                        <option value="Enojado">Enojado</option>
+                    </select>
+                </div>
+                <div className="label-input">
+                    <label htmlFor="clima" id="label-clima">
+                        ¿Cómo esta el clima?
+                    </label>
+                    <select id="clima" name="clima">
+                        <option value="" disabled selected>
+                            Clima
+                        </option>
+                        <option value="Calor">Calor</option>
+                        <option value="Frio">Frio</option>
+                        <option value="Lluvioso">Lluvioso</option>
+                        <option value="Soleado">Soleado</option>
+                    </select>
                 </div>
             </div>
+
             <div id="contenedorGeneros">
-                <p id="tituloGeneros">Selecciona hasta 3 géneros:</p>
+                <p>Selecciona hasta 3 géneros:</p>
                 <div className="generos">
-                    <button className="genero1">Rock</button>
-                    <button className="genero1">Country</button>
-                    <button className="genero1">Soul</button>
-                    <button className="genero1">Jazz</button>
-                    <button className="genero1">Blues</button>
-                    <button className="genero1">Hip-Hop</button>
-                    <button className="genero1">Pop</button>
-                    <button className="genero1">Reggae</button>
-                    <button className="genero1">Folk</button>
-                    <button className="genero1">R&B</button>
-                    <button className="genero1">Clásico</button>
-                    <button className="genero1">Alternativo</button>
-                    <button className="genero1">Ambiente</button>
-                    <button className="genero1">EDM</button>
                     <button
-                        className={isEstiloElectronica ? "electronica2" : "electronica1"}
-                        onClick={() => {
-                            setIsEstiloElectronica(!isEstiloElectronica);
-                        }}
-                        disabled={!isElectronicaEnabled}>
+                        className={`genero1 ${generoSeleccionado === "Rock" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Rock")}
+                    >
+                        Rock
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Country" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Country")}
+                    >
+                        Country
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Soul" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Soul")}
+                    >
+                        Soul
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Jazz" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Jazz")}
+                    >
+                        Jazz
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Blues" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Blues")}
+                    >
+                        Blues
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Hip-Hop" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Hip-Hop")}
+                    >
+                        Hip-Hop
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Pop" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Pop")}
+                    >
+                        Pop
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Reggae" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Reggae")}
+                    >
+                        Reggae
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Folk" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Folk")}
+                    >
+                        Folk
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "R&B" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("R&B")}
+                    >
+                        R&B
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Clasico" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Clasico")}
+                    >
+                        Clasico
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Alternativo" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Alternativo")}
+                    >
+                        Alternativo
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Ambiente" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Ambiente")}
+                    >
+                        Ambiente
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "EDM" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("EDM")}
+                    >
+                        EDM
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Electronica" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Electronica")}
+                    >
                         Electronica
                     </button>
-                    <button className="genero1">Disco </button>
-                    <button className="genero1">New Age</button>
-                    <button className="genero1">Punk</button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Disco" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Disco")}
+                    >
+                        Disco
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "New Age" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("New Age")}
+                    >
+                        New Age
+                    </button>
+                    <button
+                        className={`genero1 ${generoSeleccionado === "Punk" ? "seleccionado" : ""
+                            }`}
+                        onClick={() => handleGeneroClick("Punk")}
+                    >
+                        Punk
+                    </button>
                 </div>
             </div>
-            <Link to="/playlistMC">
-                <button
-                    disabled={!isEstiloElectronica}
-                    className={
-                        isEstiloElectronica ? "crearPlaylistEnabled" : "crearPlaylist"
-                    }>
-                    Crear Playlist
-                </button>
-            </Link>
-
+            <button id="crear-btn" onClick={handleCrearPlaylist}>
+                Crear Playlist
+            </button>
         </div>
     );
 }
